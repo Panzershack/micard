64ea9c41f866b45578d2e39b61d0c2e2312112dc
pasan <pasanim@yahoo.com>
2024-01-23 20:44:17 +0530

    added icons and containers to display user info

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.blueAccent,
         body: SafeArea(
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               CircleAvatar(
                 radius: 60.0,
@@ -20,9 +21,58 @@ class MyApp extends StatelessWidget {
               Text(
                 'Mr Fish',
                 style: TextStyle(
+                    fontFamily: 'BebasNeue',
                     fontSize: 40.0,
                     fontWeight: FontWeight.bold,
                     color: Colors.white),
+              ),
+              Text(
+                'Ocean Expert and Flutter Placeholder',
+                style: TextStyle(
+                    fontFamily: 'CrimsonText',
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.normal,
+                    color: Colors.blueAccent.shade100),
+              ),
+              Container(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                padding: EdgeInsets.all(10.0),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.phone,
+                      color: Colors.blueAccent.shade100,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      '+9477 899 999',
+                      style: TextStyle(color: Colors.blueAccent.shade100),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                padding: EdgeInsets.all(10.0),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.face,
+                      color: Colors.blueAccent.shade100,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      'Mr Fisher Sushi',
+                      style: TextStyle(color: Colors.blueAccent.shade100),
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
